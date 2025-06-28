4d43b85098213859392172a7c4621d9e3bb93efa
KrimZik <13040121+KrimZik@users.noreply.github.com>
2025-06-28 21:33:24 +0200

    Update AutoCannonAccessor.java

--- src/main/java/com/happysg/radar/mixin/AutoCannonAccessor.java
+++ src/main/java/com/happysg/radar/mixin/AutoCannonAccessor.java
@@ -1,11 +1,17 @@
 package com.happysg.radar.mixin;
 
+import com.dsvv.cbcat.cannon.heavy_autocannon.contraption.MountedHeavyAutocannonContraption;
+import com.dsvv.cbcat.cannon.twin_autocannon.contraption.MountedTwinAutocannonContraption;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import rbasamoyai.createbigcannons.cannon_control.contraption.MountedAutocannonContraption;
 import rbasamoyai.createbigcannons.cannons.autocannon.material.AutocannonMaterial;
 
-@Mixin(MountedAutocannonContraption.class)
+@Mixin({
+        MountedAutocannonContraption.class,
+        MountedTwinAutocannonContraption.class,
+        MountedHeavyAutocannonContraption.class
+})
 public interface AutoCannonAccessor {
     @Accessor(value = "cannonMaterial", remap = false)
     AutocannonMaterial getMaterial();
